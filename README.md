eacb9bf1663418ddd21a86c8fcf70fddf70e0189
Janne Lammela <lammelajanne1@gmail.com>
2023-06-28 18:33:31 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # testing_data_types
+let's explore complex datatypes and scopes!
+
+(Provided you have rustup)
+Once downloaded, navigate to the project on CMD and type:
+```
+cargo run
+```
+
+or 
+
+```
+cargo build
+cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::fmt::Debug;
+
+fn main() {
+    let test_str = "String literal";
+    let test_string = String::from("String"); //Our complex datatype
+    let number = 2;
+    let float:f32 = 2.01;
+    let tuple: (i32, i64) = (200, 300);
+
+    print_function(test_string); //Our complex data type going into print
+    println!("{test_string}"); //No longer valid: a borrowing error. Does not implement the Copy trait.
+    print_function(test_str);
+    println!("{test_str}");
+    print_function(number);
+    println!("{number}");
+    print_function(float);
+    println!("{float}");
+    print_function(tuple);
+    println!("{tuple:?}");
+}
+
+fn another_function() {
+    println!("{test_str}"); //Is no longer valid
+}
+
+fn print_function<T: Debug>(data: T) {
+    println!("{:?}", data);
+}
